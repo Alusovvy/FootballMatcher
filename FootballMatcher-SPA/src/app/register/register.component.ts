5ee64f517ef9fd5590758bc60d7374b13d4d68ca
Alusovvy <alusoowy@gmail.com>
2019-03-06 17:03:09 +0000

    Error Handling in API

--- FootballMatcher-SPA/src/app/register/register.component.ts
+++ FootballMatcher-SPA/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from 'services/auth.service';
+
+
 
 @Component({
   selector: 'app-register',
@@ -6,17 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   register() {
-    console.log(this.model);
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registration successful');
+    }, error => {
+      console.log(error);
+    });
   }
 
   cancel() {
-    console.log('canceld');
+    this.cancelRegister.emit(false);
   }
 }
