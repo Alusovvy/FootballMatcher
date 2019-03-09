54b7b345b3e3e3d71630dc802e59208a8b31b210
Alusovvy <alusoowy@gmail.com>
2019-03-09 15:34:54 +0000

    Angular user verification

--- FootballMatcher-SPA/src/app/app.module.ts
+++ FootballMatcher-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+
 import { ErrorInterceptorProvider } from '../app/services/error.interceptor';
 import { AuthService } from '../app/services/auth.service';
 import { AlertifyService } from './services/alertify.service';
@@ -25,7 +27,8 @@ import { AlertifyService } from './services/alertify.service';
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
